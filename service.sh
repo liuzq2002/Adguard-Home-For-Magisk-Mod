4c63ac37c1a00023b69183ac75f1b1bbd420faff
liuzq2002 <110004413+liuzq2002@users.noreply.github.com>
2025-10-17 08:39:52 +0800

    Add files via upload

--- service.sh
+++ service.sh
@@ -1,13 +1,37 @@
 #!/system/bin/sh
-
 SCRIPT_DIR="/data/adb/agh/scripts"
+AGH_DIR="/data/adb/agh"
+BIN_DIR="$AGH_DIR/bin"
+PID_FILE="$BIN_DIR/agh_pid"
+MAIN_LOG="$AGH_DIR/agh.log"
+export TZ='Asia/Shanghai'
 
-# 等待系统启动完成
-while [ "$(getprop init.svc.bootanim)" != "stopped" ]; do sleep 3; done
+# 所有输出重定向到日志
+exec >>"$MAIN_LOG" 2>&1
 
-# 启动 AdGuardHome 和 iptables（如需要）
-"$SCRIPT_DIR/service.sh" start
+# 启动 AdGuardHome
+export SSL_CERT_DIR="/system/etc/security/cacerts/"
+"$BIN_DIR/AdGuardHome" --no-check-update >/dev/null 2>&1 &
+echo $! > "$PID_FILE"
+sleep 2
+
+# 静默启动ModuleMOD和NoAdsService
+"$SCRIPT_DIR/ModuleMOD.sh" >/dev/null 2>&1 &
+"$SCRIPT_DIR/NoAdsService.sh" >/dev/null 2>&1 &
+
+# 验证AdGuardHome是否启动成功
+if ! kill -0 $(cat "$PID_FILE") 2>/dev/null; then
+    echo "$(date '+%F %T') AdGuardHome启动失败，尝试重启..." >> "$MAIN_LOG"
+    exec "$0"
+else
+    echo "$(date '+%F %T') AdGuardHome 启动成功。" >> "$MAIN_LOG"
+fi
+
+# 日志超限时清空
+check_and_clear_log() {
+    [ $(stat -c %s "$1" 2>/dev/null || ls -l "$1" | awk '{print $5}') -ge 102400 ] && : > "$1"
+}
+check_and_clear_log "$MAIN_LOG"
+
+# 启动 iptables
 grep -q '^enable_iptables=true' "$SCRIPT_DIR/config.sh" && "$SCRIPT_DIR/iptables.sh" enable
-
-# 启动 inotify 监听
-pgrep -f "$SCRIPT_DIR/inotify.sh" || inotifyd "$SCRIPT_DIR/inotify.sh" "$MOD_PATH:d,n" &
